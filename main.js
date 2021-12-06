859c560e05b71ab313a5a68742b93b5386782f4d
AshwiniChin <76168179+AshwiniChin@users.noreply.github.com>
2021-12-06 12:52:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  video = createCapture(300, 300);
+  video.hide();
+  classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
+
+}
+   function draw(){
+    image(video, 0, 0, 300, 300);
+   }   
+
+
+  console.log('ml5 version:', ml5.version);
+  
+
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+      
+  function check()
+  {
+    img = save('object.png');
+    classifier.classify(img, gotResult);
+  }
+
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
+}
